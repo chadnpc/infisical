f3f45d6e3ff2cdd717781e842c06e659c58f086b
alain <chadnpc@outlook.com>
2026-05-12 23:19:47 +0200

    Update infisical.Features.Tests.ps1

--- Tests/infisical.Features.Tests.ps1
+++ Tests/infisical.Features.Tests.ps1
@@ -23,8 +23,7 @@ Describe "Infisical Access Control tests " {
     $errorCaught = $false
     try {
       $client.Auth().UniversalAuth().LoginAsync("fake-id", ("fake-secret" | xconvert ToSecurestring)).GetAwaiter().GetResult() | Out-Null
-    }
-    catch {
+    } catch {
       $errorCaught = $true
     }
     $errorCaught | Should Be $true
@@ -47,8 +46,7 @@ Describe "Infisical Access Control tests " {
       $privilege = $client.Identities().AddProjectAdditionalPrivilegeAsync($addOptions).GetAwaiter().GetResult()
       $privilege | Should Not BeNullOrEmpty
     }
-  }
-  else {
+  } else {
     It "Adding Additional Privileges : Can Grant specific, scoped privileges to users and machine identities on top of their predefined roles." {
       Write-Host "Skipping: INFISICAL_MACHINE_IDENTITY_ID not set in .env"
     }
@@ -133,136 +131,130 @@ Describe "Infisical Access Control tests " {
     $secret.SecretKey | Should Be $script:newSecretName
   }
 }
-<#
+
 # https://infisical.com/docs/api-reference/endpoints/kms/
 Describe "Infisical KMS Operations" {
   Context "Keys" {
     It "List Keys [GET]" {
-      curl --request GET \
-      --url 'https://us.infisical.com/api/v1/kms/keys?limit=100&orderBy=name&orderDirection=asc'
+      # curl --request GET --url 'https://us.infisical.com/api/v1/kms/keys?limit=100&orderBy=name&orderDirection=asc'
     }
 
     It "Get Key by ID [GET]" {
-      curl --request GET \
-      --url https://us.infisical.com/api/v1/kms/keys/{keyId}
+      # curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}
     }
 
     It "Get Key by Name [GET]" {
-      curl --request GET \
-      --url https://us.infisical.com/api/v1/kms/keys/key-name/{keyName}
+      # curl --request GET --url https://us.infisical.com/api/v1/kms/keys/key-name/{keyName}
     }
 
     It "Create Key [POST]" {
-    curl --request POST \
-      --url https://us.infisical.com/api/v1/kms/keys \
-      --header 'Content-Type: application/json' \
-      --data '
-      {
-        "projectId": "<string>",
-        "name": "<string>",
-        "description": "<string>",
-        "keyUsage": "encrypt-decrypt",
-        "encryptionAlgorithm": "aes-256-gcm"
-      }
-      '
+      # curl --request POST \
+      #   --url https://us.infisical.com/api/v1/kms/keys \
+      #   --header 'Content-Type: application/json' \
+      #   --data '
+      #   {
+      #     "projectId": "<string>",
+      #     "name": "<string>",
+      #     "description": "<string>",
+      #     "keyUsage": "encrypt-decrypt",
+      #     "encryptionAlgorithm": "aes-256-gcm"
+      #   }
+      #   '
     }
 
     It "Update Key [PATCH]" {
-    curl --request PATCH \
-      --url https://us.infisical.com/api/v1/kms/keys/{keyId} \
-      --header 'Content-Type: application/json' \
-      --data '
-      {
-        "name": "<string>",
-        "isDisabled": true,
-        "description": "<string>"
-      }
-      '
+      # curl --request PATCH \
+      #   --url https://us.infisical.com/api/v1/kms/keys/{keyId} \
+      #   --header 'Content-Type: application/json' \
+      #   --data '
+      #   {
+      #     "name": "<string>",
+      #     "isDisabled": true,
+      #     "description": "<string>"
+      #   }
+      #   '
     }
 
     It "Delete Key [DEL]" {
-      curl --request DELETE --url https://us.infisical.com/api/v1/kms/keys/{keyId}
-
+      # curl --request DELETE --url https://us.infisical.com/api/v1/kms/keys/{keyId}
     }
 
     It "Retrieve Public Key [GET]" {
-      curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}/public-key
+      # curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}/public-key
     }
 
     It "Export Private Key [GET]" {
-       curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}/private-key
+      # curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}/private-key
     }
 
     It "Bulk Export Private Keys [POST]" {
-      curl --request POST \
-      --url https://us.infisical.com/api/v1/kms/keys/bulk-export-private-keys \
-      --header 'Content-Type: application/json' \
-      --data '
-      {
-        "keyIds": [
-          "3c90c3cc-0d44-4b50-8888-8dd25736052a"
-        ]
-      }
-      '
+      # curl --request POST \
+      #   --url https://us.infisical.com/api/v1/kms/keys/bulk-export-private-keys \
+      #   --header 'Content-Type: application/json' \
+      #   --data '
+      #   {
+      #     "keyIds": [
+      #       "3c90c3cc-0d44-4b50-8888-8dd25736052a"
+      #     ]
+      #   }
+      #   '
     }
   }
 
   Context "Encryption" {
     It "Encrypt Data [POST]" {
-    curl --request POST \
-        --url https://us.infisical.com/api/v1/kms/keys/{keyId}/encrypt \
-        --header 'Content-Type: application/json' \
-        --data '
-      {
-        "plaintext": "<string>"
-      }
-      '
+      # curl --request POST \
+      #     --url https://us.infisical.com/api/v1/kms/keys/{keyId}/encrypt \
+      #     --header 'Content-Type: application/json' \
+      #     --data '
+      #   {
+      #     "plaintext": "<string>"
+      #   }
+      #   '
     }
 
     It "Decrypt Data [POST]" {
-    curl --request POST \
-        --url https://us.infisical.com/api/v1/kms/keys/{keyId}/decrypt \
-        --header 'Content-Type: application/json' \
-        --data '
-      {
-        "ciphertext": "<string>"
-      }
-      '
+      # curl --request POST \
+      #     --url https://us.infisical.com/api/v1/kms/keys/{keyId}/decrypt \
+      #     --header 'Content-Type: application/json' \
+      #     --data '
+      #   {
+      #     "ciphertext": "<string>"
+      #   }
+      #   '
     }
   }
 
   Context "Signing" {
     It "Sign Data [POST]" {
-    curl --request POST \
-        --url https://us.infisical.com/api/v1/kms/keys/{keyId}/sign \
-        --header 'Content-Type: application/json' \
-        --data '
-      {
-        "signingAlgorithm": "RSASSA_PSS_SHA_512",
-        "data": "<string>",
-        "isDigest": false
-      }
-      '
+      # curl --request POST \
+      #     --url https://us.infisical.com/api/v1/kms/keys/{keyId}/sign \
+      #     --header 'Content-Type: application/json' \
+      #     --data '
+      #   {
+      #     "signingAlgorithm": "RSASSA_PSS_SHA_512",
+      #     "data": "<string>",
+      #     "isDigest": false
+      #   }
+      #   '
     }
 
     It "Verify Signature [POST]" {
-    curl --request POST \
-        --url https://us.infisical.com/api/v1/kms/keys/{keyId}/verify \
-        --header 'Content-Type: application/json' \
-        --data '
-      {
-        "data": "<string>",
-        "signature": "<string>",
-        "signingAlgorithm": "RSASSA_PSS_SHA_512",
-        "isDigest": false
-      }
-      '
+      # curl --request POST \
+      #     --url https://us.infisical.com/api/v1/kms/keys/{keyId}/verify \
+      #     --header 'Content-Type: application/json' \
+      #     --data '
+      #   {
+      #     "data": "<string>",
+      #     "signature": "<string>",
+      #     "signingAlgorithm": "RSASSA_PSS_SHA_512",
+      #     "isDigest": false
+      #   }
+      #   '
     }
 
     It "List Signing Algorithms [GET]" {
-      curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}/signing-algorithms
+      # curl --request GET --url https://us.infisical.com/api/v1/kms/keys/{keyId}/signing-algorithms
     }
   }
 }
-
-#>
